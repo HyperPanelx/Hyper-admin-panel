f9e5fd58fbf1d04082c4c8ae5feaf774fa2ebf74
hooman mousavi <hoomanmousavi77@gmail.com>
2023-05-24 22:26:19 +0330

    navbar >> fullscreen added

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -7,6 +7,8 @@
     <div class="navbar-right">
       <NavbarNotification/>
       <NavbarTheme />
+      <Icon @click="requestFullScreen" :name="fullScreenFlag ? 'solar:quit-full-screen-linear' : 'solar:full-screen-bold'" class="navbar-icon" size="1.6rem"
+      />
     </div>
   </nav>
 </template>
@@ -14,10 +16,24 @@
 <script setup lang="ts">
 const sidebarCollapseFlag=useState('sidebarCollapseFlag',()=>false)
 const windowWidth:null|number=process.client ? window.innerWidth : null
+const fullScreenFlag=ref<boolean>(false)
 
 const collapseSidebar = () => {
   sidebarCollapseFlag.value=!sidebarCollapseFlag.value
 }
+const requestFullScreen = () => {
+  if(process.client && document.documentElement.requestFullscreen){
+    fullScreenFlag.value=!fullScreenFlag.value
+    if(fullScreenFlag.value){
+      document.documentElement.requestFullscreen()
+    }else{
+      document.exitFullscreen()
+    }
+
+
+  }
+}
+
 onMounted(()=>{
   if(windowWidth && windowWidth>500 && windowWidth<960){
     sidebarCollapseFlag.value=true
